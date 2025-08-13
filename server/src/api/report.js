795441edec1acadbd1a9376ca02b41398b2c76bd
Jose Roman <manitogames24@gmail.com>
2025-08-13 14:17:00 -0400

    fix report getFiles import

--- server/src/api/report.js
+++ server/src/api/report.js
@@ -71,7 +71,7 @@ router.get("/medical-guide/:id", async function (req, res, next) {
           } else {
               doctor.speciality = []
           }
-          const [doctor_profile] = await _upload.getFiles({ ref_key: 't_doctor', ref_id: doctor.id });
+          const [doctor_profile] = await _query.getFiles({ ref_key: 't_doctor', ref_id: doctor.id });
           if (doctor_profile) {
               data._files.doctor.push({ ...doctor, profile: doctor_profile.url })
           }
@@ -88,7 +88,7 @@ router.get("/medical-guide/:id", async function (req, res, next) {
     let provider_files;
     if (data.provider) {
       data.provider.place = `${data.provider.description}, ${data.provider.country}, ${data.provider.city}`
-      provider_files = await _upload.getFiles({ ref_key: 't_provider', ref_id: data.provider.id });
+      provider_files = await _query.getFiles({ ref_key: 't_provider', ref_id: data.provider.id });
 
       if (provider_files?.length) {
           const [provider_file] = provider_files.filter(i => i.$file_type_id === 198)
@@ -99,7 +99,7 @@ router.get("/medical-guide/:id", async function (req, res, next) {
       data._files.provider = await _upload.getProfilePic({ ref_key: 't_provider', ref_id: item.provider_id });
     }
 
-    const files = await _upload.getFiles({ ref_key: 't_event', ref_id: req.params.id });
+    const files = await _query.getFiles({ ref_key: 't_event', ref_id: req.params.id });
 
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
@@ -185,7 +185,7 @@ router.get("/medical-guide2/:id", async function (req, res, next) {
       user,
     });
 
-    const files = await _upload.getFiles({ ref_key: "t_event", ref_id: req.params.id });
+    const files = await _query.getFiles({ ref_key: "t_event", ref_id: req.params.id });
 
     const { docDefinition } = await generateMedicalGuideDoc({ item, itineraries, provider, files, account, user })
 
